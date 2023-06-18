b7a38eff47b91838c0a028402171cef57b1f7b3a
Jhon Garcia <jgarcia@lsv-tech.com>
2023-06-18 11:51:08 -0500

    [ ✨ ADD: Register models - administration panel ]

--- apps/importer_app/admin.py
+++ apps/importer_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, Teacher, Course, Grade
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Grade)
+
+

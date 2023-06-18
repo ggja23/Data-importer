4ba044fe1471d0a9afd6175ea40f26082d93af1d
Jhon Garcia <jgarcia@lsv-tech.com>
2023-06-18 11:44:40 -0500

    [ ⚙️ ADD: 'importer_app' into 'importer_settings_project'  ]

--- apps/importer_app/apps.py
+++ apps/importer_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ImporterAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'importer_app'
+    name = 'apps.importer_app'
